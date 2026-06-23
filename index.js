8a31cf30b23c86a4a391005190497a89341c2802
Thanh Nguyen <nduythanh357@gmail.com>
2026-06-23 11:51:56 -0500

    implement the /create route

--- index.js
+++ index.js
@@ -4,13 +4,33 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
+let posts = [];
+let nextId = 1;
+
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
-    res.render("index.ejs");
+    res.render("index", { posts });
 });
- 
+
+app.get('/create', (req, res) => {
+    res.render("create");
+});
+
+app.post('/create', (req, res) => {
+    const {title, content} = req.body;
+    const newPost = {
+        id : nextId++,
+        title,
+        content
+    };
+    posts.push(newPost);
+    res.redirect('/');
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
